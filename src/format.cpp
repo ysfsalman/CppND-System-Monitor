657cce5c75234cb8fe88dfff7ceef5694ed195db
woycoy <yusufsalman@hotmail.co.id>
2020-06-26 18:37:32 +0200

    add time format functionality

--- src/format.cpp
+++ src/format.cpp
@@ -3,9 +3,13 @@
 #include "format.h"
 
 using std::string;
-
-// TODO: Complete this helper function
-// INPUT: Long int measuring seconds
-// OUTPUT: HH:MM:SS
-// REMOVE: [[maybe_unused]] once you define the function
-string Format::ElapsedTime(long seconds[[maybe_unused]]) { return string(); }
+using std::to_string;
+string Format::ElapsedTime(long seconds) { 
+    long HH, MM, SS;
+    HH = seconds/3600;
+    seconds %= 3600;
+    MM = seconds/60;
+    seconds %= 60;
+    SS = seconds;
+    return to_string(HH) + ":" + to_string(MM) + ":" + to_string(SS);
+}
